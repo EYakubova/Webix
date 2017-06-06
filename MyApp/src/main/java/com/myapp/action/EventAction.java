22fc52be305b8a7ab30c14148da2bb10afab32f5
Yakubova Elvira <yaakubova@yandex.ru>
2017-06-06 08:18:58 +0300

    Clear class

--- MyApp/src/main/java/com/myapp/action/EventAction.java
+++ MyApp/src/main/java/com/myapp/action/EventAction.java
@@ -9,9 +9,6 @@ import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class EventAction extends ActionSupport {
-	////lllhhh
-	//Second commit
-	
 	
 	private List<Event> data = new ArrayList<Event>();
 	private String eventId;
